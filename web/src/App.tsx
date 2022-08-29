42358b82f46bccb7402dc2afbb9f08dcf9f2d651
alexandr <perfiliev29@yandex.ru>
2022-08-29 19:10:45 +0300

    web: custom eslint settings

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,14 +1,13 @@
-import React from 'react';
 import { Provider } from 'react-redux';
 
 import store from './store';
 
 function App() {
-    return (
-        <Provider store={store}>
-            Hello!!!!
-        </Provider>
-    );
+  return (
+    <Provider store={store}>
+      Hello!!!!
+    </Provider>
+  );
 }
 
 export default App;
